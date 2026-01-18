8f7d37b49c0cec3b875010e24151b80428569f81
Vedvart98 <yadavvedvart@gmail.com>
2026-01-18 23:48:43 +0530

    update RAG file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const faqs = require('faqs.json')
+const faqs = require('./faqs.json')
 const app = express();
 app.use(express.json());
 
